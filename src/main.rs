672e27f039678bf95b1cb5c6d6429215668c3cb4
Denis Novikov <alpden550@gmail.com>
2024-12-10 18:42:24 +0300

    add execute command

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    repl();
+}
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+fn repl() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    execute_command(input.trim());
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        execute_command(input.trim());
+    }
 }
 
 fn execute_command(command: &str) {
     match command {
         _ => {
-            println!("{}: not found", command);
+            println!("{}: command not found", command);
         }
     }
 }
