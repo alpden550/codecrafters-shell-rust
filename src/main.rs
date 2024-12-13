9c327961c6b2dcef3df9ed14f3e03baa37d72697
Denis Novikov <alpden550@gmail.com>
2024-12-13 13:06:10 +0300

    add exec command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod constants;
 mod enums;
 
 use enums::BuiltInCommand;
+use std::path::PathBuf;
 use std::{
+    fs,
     io::{self, Write},
     path::Path,
 };
@@ -26,7 +28,9 @@ fn repl() {
 }
 
 fn execute_command(command: &str) {
-    let path = std::env::var("PATH").unwrap();
+    if execute_external_command(command).is_ok()  {
+        return;
+    }
 
     match command.parse::<BuiltInCommand>() {
         Ok(BuiltInCommand::Exit(code)) => std::process::exit(code),
@@ -35,15 +39,53 @@ fn execute_command(command: &str) {
             if constants::BUILTINS.contains(&c.as_str()) {
                 println!("{} is a shell builtin", c);
             } else {
-                let found = path.split(":").find(|dir| Path::new(dir).join(&c).exists());
-                match found {
-                    Some(dir) => {
-                        println!("{} is {}", c, Path::new(dir).join(&c).display());
-                    }
-                    None => println!("{}: not found", c),
+                match check_executable(c.as_str()) {
+                    Ok(p) => println!("{} is {}", c, p.display()),
+                    Err(_) => println!("{}: not found", c),
                 }
             }
         }
-        Err(_) => println!("{}: command not found", command),
+        Err(_) => println!("{}: not found", command),
     }
+}
+
+fn execute_external_command(args: &str) -> Result<(), String> {
+    let mut parts = args.split_whitespace();
+    let command = parts.next().unwrap_or("");
+    let arguments: Vec<&str> = parts.collect();
+
+    match check_executable(command) {
+        Ok(path) => {
+            let status = std::process::Command::new(path)
+                .args(arguments)
+                .status()
+                .expect("Failed to execute command");
+
+            if !status.success() {
+                eprintln!("{}: command failed with status {}", command, status);
+            }
+            Ok(())
+        }
+        Err(_) => Err("command not found".to_string()),
+    }
+}
+
+fn check_executable(command: &str) -> Result<PathBuf, String> {
+    let env_path = std::env::var("PATH").map_err(|_| "Failed to read PATH environment variable")?;
+
+    env_path
+        .split(':')
+        .filter_map(|dir| {
+            let path = Path::new(dir).join(command);
+            if fs::metadata(&path)
+                .map(|metadata| metadata.is_file())
+                .unwrap_or(false)
+            {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .next()
+        .ok_or_else(|| format!("{}: command not found", command))
 }
