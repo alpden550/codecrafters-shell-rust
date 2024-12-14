eb8413c51e62c98b7723ee75d3151e13093c7da7
Denis Novikov <alpden550@gmail.com>
2024-12-14 16:34:15 +0300

    refact(src): move to helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,5 @@
+use std::{env, fs};
+use std::path::{Path, PathBuf};
 use shellwords::split;
 
 pub fn parse_shell_words(text: &str) -> Vec<String> {
@@ -7,4 +9,48 @@ pub fn parse_shell_words(text: &str) -> Vec<String> {
     });
 
     words
+}
+
+pub fn check_executable(command: &str) -> Result<PathBuf, String> {
+    let env_path = env::var("PATH").map_err(|_| "Failed to read PATH environment variable")?;
+
+    env_path
+        .split(':')
+        .filter_map(|dir| {
+            let path = Path::new(dir).join(command);
+            if fs::metadata(&path)
+                .map(|metadata| metadata.is_file())
+                .unwrap_or(false)
+            {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .next()
+        .ok_or_else(|| format!("{}: command not found", command))
+}
+
+pub fn execute_external_command(command: &str) -> Result<(), String> {
+    let parts = parse_shell_words(command);
+    if parts.is_empty() {
+        return Err("empty command".to_string());
+    }
+    let cmd = parts[0].as_str();
+    let args = &parts[1..];
+
+    match check_executable(cmd) {
+        Ok(path) => {
+            let status = std::process::Command::new(path)
+                .args(args)
+                .status()
+                .expect("Failed to execute command");
+
+            if !status.success() {
+                eprintln!("{}: command failed with status {}", cmd, status);
+            }
+            Ok(())
+        }
+        Err(_) => Err("command not found".to_string()),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ mod constants;
 mod enums;
 mod helpers;
 
+use crate::helpers::{check_executable, execute_external_command};
 use enums::BuiltInCommand;
-use helpers::parse_shell_words;
 use std::path::PathBuf;
 use std::{
-    env, fs,
+    env,
     io::{self, Write},
-    path::Path,
 };
 
 fn main() {
@@ -71,48 +70,4 @@ fn execute_command(command: &str) {
             }
         }
     }
-}
-
-fn execute_external_command(command: &str) -> Result<(), String> {
-    let parts = parse_shell_words(command);
-    if parts.is_empty() {
-        return Err("empty command".to_string());
-    }
-    let cmd = parts[0].as_str();
-    let args = &parts[1..];
-
-    match check_executable(cmd) {
-        Ok(path) => {
-            let status = std::process::Command::new(path)
-                .args(args)
-                .status()
-                .expect("Failed to execute command");
-
-            if !status.success() {
-                eprintln!("{}: command failed with status {}", cmd, status);
-            }
-            Ok(())
-        }
-        Err(_) => Err("command not found".to_string()),
-    }
-}
-
-fn check_executable(command: &str) -> Result<PathBuf, String> {
-    let env_path = env::var("PATH").map_err(|_| "Failed to read PATH environment variable")?;
-
-    env_path
-        .split(':')
-        .filter_map(|dir| {
-            let path = Path::new(dir).join(command);
-            if fs::metadata(&path)
-                .map(|metadata| metadata.is_file())
-                .unwrap_or(false)
-            {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .next()
-        .ok_or_else(|| format!("{}: command not found", command))
 }
